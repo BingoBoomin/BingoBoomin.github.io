5291708b66af24cc0bc77de3d0b686281e57df86
BingoBoomin <51534523+BingoBoomin@users.noreply.github.com>
2019-06-18 19:43:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-kzkzk
+function hamburgerClick() {
+    var mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+    var display = mobileMenu.style.display;
+    if (display == "") {
+        mobileMenu.style.display = "flex";
+    } else {
+        mobileMenu.style.display = "";
+    }
+}
+
+var hamburgerIcon = document.getElementsByClassName("menu-icon")[0];
+hamburgerIcon.addEventListener("click", hamburgerClick);
